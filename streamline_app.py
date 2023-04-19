d6e013894f08d9af31b5e33909eed31cf91cc5e7
dataisc0000l <131291627+dataisc0000l@users.noreply.github.com>
2023-04-19 15:14:58 -0700

    Added second text entry box

--- streamline_app.py
+++ streamline_app.py
@@ -42,3 +42,7 @@ my_cur.execute("SELECT * FROM pc_rivery_db.public.fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
+
+# Second text entry box
+add_my_fruit = streamlit.text_input('What fruit would you like add?','Kiwi')
+streamlit.write('Thanks for adding ', add_my_fruit)

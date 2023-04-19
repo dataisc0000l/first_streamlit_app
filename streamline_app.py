ed45270ff7ed8939616641ff0b627929710348fd
dataisc0000l <131291627+dataisc0000l@users.noreply.github.com>
2023-04-19 13:52:41 -0700

    Update streamline_app.py
    Normalizing the JSON data and outputting it as a table

--- streamline_app.py
+++ streamline_app.py
@@ -26,4 +26,9 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+streamlit.text(fruityvice_response.json()) # Just writes the data to the screen
+
+# Take the JSON version of the response and normalize it
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# Outputs the data as a table
+streamlit.dataframe(fruityvice_normalized

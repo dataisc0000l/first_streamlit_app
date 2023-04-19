740d7184708f37bd6fcc719dce84baf3c5d3af93
dataisc0000l <131291627+dataisc0000l@users.noreply.github.com>
2023-04-19 15:26:37 -0700

    Updated Try-Except statement

--- streamline_app.py
+++ streamline_app.py
@@ -27,9 +27,9 @@ streamlit.dataframe(fruits_to_show)
 # New Section to display Fruityvice API Response
 streamlit.header("Fruityvice Fruit Advice!")
 try:
-  fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
-     streamlit.write('The user entered ', fruit_choice)
+     streamlit.error("Please select a fruit to get information.")
   else:
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
     # streamlit.text(fruityvice_response.json()) # Just writes the data to the screen
